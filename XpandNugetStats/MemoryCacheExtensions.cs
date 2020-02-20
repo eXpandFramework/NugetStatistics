dfd84fad5f942ed2f329e099a5b5a19eefce751e
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-20 07:44:05 +0200

    ClearCache not working

--- XpandNugetStats/MemoryCacheExtensions.cs
+++ XpandNugetStats/MemoryCacheExtensions.cs
@@ -1,38 +1,30 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Primitives;
 
 namespace XpandNugetStats{
+    
     public static class MemoryCacheExtensions{
-        private static readonly ConcurrentDictionary<IMemoryCache, ConcurrentDictionary<string, CancellationTokenSource>> Memories =
-                new ConcurrentDictionary<IMemoryCache, ConcurrentDictionary<string, CancellationTokenSource>>();
+        private static readonly ConcurrentDictionary<string,CancellationTokenSource> TokenSources=new ConcurrentDictionary<string, CancellationTokenSource>();
+
 
         public static IObservable<T> GetOrCreate<T>(this IMemoryCache memoryCache,  string key,Func<ICacheEntry, IObservable<T>> factory,
             int timeout, Func<int, TimeSpan> time = null){
-
-            return memoryCache.GetOrCreateAsync(key, entry => entry.CacheItem(memoryCache, key, factory(entry), timeout, time))
-                .ToObservable();
+            return Observable.FromAsync(() => memoryCache.GetOrCreateAsync(key,
+                async entry => await entry.CacheItem(memoryCache, key, Observable.Defer(() => factory(entry)), timeout, time)));
         }
 
-        public static void ClearCache(this IMemoryCache memoryCache, string key=null){
-            if (Memories.TryGetValue(memoryCache, out var tokens)){
-                if (key!=null){
-                    if (tokens.TryGetValue(key, out var token)){
-                        token.Cancel();
-                    }
-                }
-                else{
-                    foreach (var tokenKey in tokens.Keys){
-                        if (tokens.TryGetValue(tokenKey, out var value)){
-                            value.Cancel();
-                        }
-                    }
-                }
+        public static void ClearCache(this IMemoryCache memoryCache){
+            var cancellationTokenSources = TokenSources.Values.ToArray();
+            TokenSources.Clear();
+            foreach (var tokenSource in cancellationTokenSources){
+                tokenSource.Cancel();
             }
         }
 
@@ -41,19 +33,16 @@ namespace XpandNugetStats{
             time ??= (i => TimeSpan.FromMinutes(i));
             cacheEntry.AbsoluteExpirationRelativeToNow = time(timeout);
             var result = await factory;
-
-            var cacheEntryOptions = new MemoryCacheEntryOptions();
-            memoryCache.Set(key, result, cacheEntryOptions);
-            cacheEntryOptions.RegisterPostEvictionCallback(async (keyArg, o, reason, state) =>
-                await cacheEntry.CacheItem(memoryCache, key, factory, timeout, time));
-            var tokenSources = Memories.GetOrAdd(memoryCache, cache => new ConcurrentDictionary<string, CancellationTokenSource>());
             var tokenSource = new CancellationTokenSource(time(timeout-5));
+            TokenSources.TryAdd(key, tokenSource);
             var expirationToken = new CancellationChangeToken(tokenSource.Token);
-            tokenSources.AddOrUpdate(key, s => tokenSource, (s, source) => tokenSource );
-            cacheEntryOptions.AddExpirationToken(expirationToken);
-            
-            
-
+            cacheEntry.SlidingExpiration=time(timeout);
+            cacheEntry.ExpirationTokens.Add(expirationToken);
+            cacheEntry.PostEvictionCallbacks.Add(new PostEvictionCallbackRegistration(){EvictionCallback =
+                (o, value, reason, state) => {
+                    memoryCache.GetOrCreate(key, entry => Observable.Defer(() => factory), timeout, time).ObserveOn(ImmediateScheduler.Instance)
+                        .SubscribeOn(ImmediateScheduler.Instance).Subscribe();
+                }});
             return result;
         }
     }
